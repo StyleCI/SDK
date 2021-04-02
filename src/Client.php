21d5346f714570cbc82a4b91eb72d54f1d806e93
Graham Campbell <hello@gjcampbell.co.uk>
2021-04-02 11:29:57 +0100

    Updates

--- src/Client.php
+++ src/Client.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the StyleCI SDK.
  *
- * (c) Alt Three Services Limited
+ * (c) Graham Campbell Technology Ltd
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
